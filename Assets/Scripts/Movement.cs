007b7761e2964fe65ed370db8bbec9ca1429d1cc
Jorikos <26204@ma-web.nl>
2021-06-17 09:44:00 +0200

    Movement basics

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    private GameObject playerObject;
+    private float movementSpeed;
+    private float jumpCooldown;
+    private float jumpHeight;
+    private float timer;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        playerObject = GameObject.Find("Player");
+        playerRB = playerObject.GetComponent<Rigidbody>();
+        movementSpeed = 5f;
+        jumpHeight = 50f;
+        isGrounded = true;
+        jumpCooldown = 1.5f;
+    }
+
+    private void Update()
+    {
+        if (!isGrounded)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer >= jumpCooldown)
+        {
+            timer = 0;
+            isGrounded = true;
+        }
+
+        Debug.Log(isGrounded);   
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerObject.transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerObject.transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerObject.transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerObject.transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            playerObject.(Vector3.up * jumpHeight * Time.deltaTime);
+            isGrounded = false;
+        }
+    }
+}
