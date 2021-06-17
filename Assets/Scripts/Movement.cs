6b0b6ee33a008f150ff215e4ce91d4080d38592e
Jorikos <26204@ma-web.nl>
2021-06-17 10:38:04 +0200

    Double jump

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,34 +11,37 @@ public class Movement : MonoBehaviour
     private float jumpHeight;
     private float timer;
     private bool isGrounded;
+    private bool canJump;
+    private bool hasDoubleJumped;
 
     private void Awake()
     {
         playerObject = GameObject.Find("Player");
         playerRB = playerObject.GetComponent<Rigidbody>();
         movementSpeed = 5f;
-        jumpHeight = 50f;
-        isGrounded = true;
-        jumpCooldown = 1.5f;
+        jumpHeight = 500f;
+        canJump = true;
+        jumpCooldown = 1.2f;
     }
 
     private void Update()
     {
-        if (!isGrounded)
+        //Check if jumpCooldown has expired and is able to jump again
+        if (!canJump)
         {
             timer += Time.deltaTime;
         }
         if (timer >= jumpCooldown)
         {
             timer = 0;
-            isGrounded = true;
+            canJump = true;
         }
-
-        Debug.Log(isGrounded);   
     }
 
     private void FixedUpdate()
     {
+        Debug.Log(hasDoubleJumped);
+
         if (Input.GetKey(KeyCode.W))
         {
             playerObject.transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
@@ -56,10 +59,30 @@ public class Movement : MonoBehaviour
             playerObject.transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
-            playerRB.AddForce(Vector3.up * jumpHeight * Time.deltaTime);
-            isGrounded = false;
+            if (!isGrounded && !hasDoubleJumped)
+            {
+                playerRB.AddForce(Vector3.up * jumpHeight * Time.deltaTime, ForceMode.Impulse);
+                hasDoubleJumped = true;
+                canJump = false;
+            }
+            else if (isGrounded && !hasDoubleJumped)
+            {
+                playerRB.AddForce(Vector3.up * jumpHeight * Time.deltaTime, ForceMode.Impulse);
+                isGrounded = false;
+                canJump = false;
+            }
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "JumpableSurface")
+        {
+            hasDoubleJumped = false;
+            isGrounded = true;
         }
     }
 }
